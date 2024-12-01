49c150014106577d99120cc0ca66067df018e447
Hagi T <marspeoplehg@gmail.com>
2024-12-01 12:09:52 +0400

    AppProviderの作成とURIのセット(228)

---
+++ app/src/main/java/enjoyhac/jumpingstone/tasktimer/AppProvider.kt
@@ -0,0 +1,78 @@
+package enjoyhac.jumpingstone.tasktimer
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.util.Log
+
+/**
+ * TaskTimerアプリのプロバイダー
+ *
+ * [AppDatabase]のことを知っている唯一のクラス
+ */
+
+private const val TAG = "AppProvider"
+
+private const val CONTENT_AUTHORITY = "enjoyhac.jumpingstone.tasktimer.provider"
+
+private const val TASKS = 100
+private const val TASKS_ID = 101
+
+private const val TIMINGS = 200
+private const val TIMINGS_ID = 201
+
+private const val TASK_DURATIONS = 400
+private const val TASK_DURATIONS_ID = 401
+
+val CONTENT_AUTHORITY_URI: Uri = Uri.parse("content://$CONTENT_AUTHORITY")
+
+class AppProvider: ContentProvider() {
+
+    private val uriMatcher by lazy { buildUriMatcher() }
+
+    private fun buildUriMatcher() : UriMatcher {
+        Log.d(TAG, "buildUrlMatcher starts")
+        val matcher = UriMatcher(UriMatcher.NO_MATCH)
+
+        return matcher
+    }
+
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreate(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(uri: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+}
